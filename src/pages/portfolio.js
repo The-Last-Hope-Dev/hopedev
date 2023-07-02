2818b9655d874f65229cd2ce6ed98869217db185
Yeison Garzon T <garzontoroyeison@gmail.com>
2023-07-02 00:45:28 -0500

    main: test deploy netlify

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,7 +3,7 @@ import Layout from "@/components/layout";
 export default function Portfolio() {
     return (
         <Layout>
-            <h1>Portfolio 2</h1>
+            <h1>Portfolio test</h1>
         </Layout>
     )
 }
