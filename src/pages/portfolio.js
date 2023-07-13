ad94d5ff28f6b4c76a035ef4cfe18333fe629a29
Yeison Garzon T <garzontoroyeison@gmail.com>
2023-07-13 00:06:58 -0500

    MAIN: main change

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,7 +3,7 @@ import Layout from "@/components/layout";
 export default function Portfolio() {
     return (
         <Layout>
-            <h1>Portfolio test</h1>
+            <h1>Portfolio test deploy</h1>
         </Layout>
     )
 }
